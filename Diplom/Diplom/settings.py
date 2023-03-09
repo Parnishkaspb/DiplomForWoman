c631d2d0509197f3d2538a642436af95bf2f2729
Богдан Красников <bogdankrasnikov@MacBook-Pro-Bogdan.local>
2023-03-09 17:23:45 +0300

    first commit

--- Diplom/Diplom/settings.py
+++ Diplom/Diplom/settings.py
@@ -119,7 +119,7 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'mysite/static'),
+    os.path.join(BASE_DIR, 'Diplom/static'),
 ]
 
 # Default primary key field type
--- Diplom/appForDiplom/views.py
+++ Diplom/appForDiplom/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
+# def index(request):
+    # cookie = HttpResponse('<h4>Проверка!</h4>')
+    # cookie.set_cookie('personal_id', 123)
+    # return cookie
+
+# def index(request):
+#     show = request.COOKIES['personal_id']
+#     return HttpResponse(f'<h4>Проверка! На куках: {show}</h4>')
+
 def index(request):
-    return HttpResponse('<h4>Проверка</h4>')
+    pass
