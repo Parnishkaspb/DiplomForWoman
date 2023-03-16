88ffd001a44b97b9aeac3896fa738bebf42aa1be
Богдан Красников <bogdankrasnikov@MacBook-Pro-Bogdan.local>
2023-03-16 17:24:46 +0300

    comment

--- Diplom/appForDiplom/views.py
+++ Diplom/appForDiplom/views.py
@@ -11,22 +11,35 @@ from django.http import HttpResponse
 #     show = request.COOKIES['personal_id']
 #     return HttpResponse(f'<h4>Проверка! На куках: {show}</h4>')
 
+# def index(request):
+#     json_string = {
+#         "text":"<h1> Добро пожаловать </h1>", 
+#         "task": [
+#             {
+#                 'number': '1',
+#                 'q':'Когда был основан ГУАП?',
+#                 'var': [ '2222', '2221', '2223', '2224'],
+#                 'answer': '2223'
+#             }, 
+#             {
+#                 'number': '2',
+#                 'q':'Когда был основан Питер?',
+#                 'var': [ '1703', '1702', '1700', '1701'],
+#                 'answer': '1703'
+#             }, 
+#         ]
+#     }
+#     return render(request, 'appForDiplom/lab.html', {'json_string': json_string})
+
 def index(request):
     json_string = {
         "text":"<h1> Добро пожаловать </h1>", 
         "task": [
             {
                 'number': '1',
-                'q':'Когда был основан ГУАП?',
-                'var': [ '2222', '2221', '2223', '2224'],
-                'answer': '2223'
-            }, 
-            {
-                'number': '2',
-                'q':'Когда был основан Питер?',
-                'var': [ '1703', '1702', '1700', '1701'],
-                'answer': '1703'
-            }, 
+                'q':'Создать таблицу под названием <strong>DB</strong> следующей структуры: <table border="1" cellpadding="1" cellspacing="1"><tr><td>Поле</td><td>Тип, описание</td></tr><tr><td><strong>ID</strong></td><td>INT PRIMARY KEY AUTO_INCREMENT</td></tr></table>'
+            }
         ]
     }
-    return render(request, 'appForDiplom/lab.html', {'json_string': json_string})
+    
+    return render(request, 'appForDiplom/prac.html', {'json_string': json_string})
